7b1723832338b0e165f95249a2f0d873dab776ae
jejaksaid <said@fastlearningcenter.com>
2021-08-18 08:06:38 +0700

    ]

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User was logged out')
+    messages.info(request, 'User was logged out')
     return redirect('login')
 
 def registerUser(request):
@@ -72,5 +72,16 @@ def userProfile(request, pk):
     otherSkills = profile.skill_set.filter(description="")
 
     context = {'profile': profile, 'topSkills': topSkills,
-               "otherSkills": otherSkills}
+               'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+    return render(request, 'users/account.html', context)
