ea5de411ec9befa56d5d3b26b4e7064c0946d719
jejaksaid <said@fastlearningcenter.com>
2021-08-19 08:41:07 +0700

    user edit account

--- users/signals.py
+++ users/signals.py
@@ -16,6 +16,15 @@ def createProfile(sender, instance, created, **kwargs):
             name=user.first_name,
         )
 
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
 
 
 def deleteUser(sender, instance, **kwargs):
@@ -24,5 +33,6 @@ def deleteUser(sender, instance, **kwargs):
 
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
 
